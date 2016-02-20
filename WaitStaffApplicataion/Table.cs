39b9e0b6557fd32df3b3fdb6aac6f99f4070a68e
Alex Reinemann <Reineman18@up.edu>
2016-02-20 13:49:27 -0800

    Added Table class Added farmwork for Receipt and Menu classes

---
+++ WaitStaffApplicataion/Table.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/**
+* Name: Table 
+* Description:
+*   The Table class is used to track one tables status and receipts.
+* 
+* Authors: Alex Reinemann
+* Date created: 2/20/2016
+*/
+namespace WaitStaffApplicataion
+{
+    class Table
+    {
+        int iNumPeople,
+            iTableStatus,
+            iFoodStatus;
+        Receipt receipt;
+        String sSpecial;
+        Menu menu;
+
+        void updateTableStatus()
+        {
+            iTableStatus++;
+
+            if (iTableStatus > 2)
+                iTableStatus = 0;
+        }
+
+        void updateFoodStatus(int newStatus)
+        {
+            iFoodStatus = newStatus;
+        }
+
+        //setter methods
+
+        void setNumPeople(int people)
+        {
+            iNumPeople = people;
+        }
+
+        //getter methods
+
+        int getTableStatus()
+        {
+            return iTableStatus;
+        }
+
+        int getFoodStatus()
+        {
+            return iFoodStatus;
+        }
+    }
+}
