cfd8b38886fc718d1d8cc71001d4a4f0278ae79a
taylorg18 <taylorg18@up.edu>
2016-02-20 15:01:24 -0800

    Redo Table form

---
+++ WaitStaffApplicataion/TableForm2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WaitStaffApplicataion
+{
+    public partial class TableForm2 : Form
+    {
+        public TableForm2()
+        {
+            InitializeComponent();
+        }
+    }
+}
