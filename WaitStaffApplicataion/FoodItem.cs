e320dcf53d6690df62851a6a8442d1c375989d90
Alex Reinemann <Reineman18@up.edu>
2016-02-20 14:07:49 -0800

    Added methods and atributes to the FoodItem, FoodMenu, Receipt, and Table classes.

---
+++ WaitStaffApplicataion/FoodItem.cs
@@ -0,0 +1,15 @@
+﻿namespace WaitStaffApplicataion
+{
+    internal class FoodItem
+    {
+        string name,
+               special;
+        double price;
+        bool bAvaliability;
+
+        public bool getAvaliability()
+        {
+            return bAvaliability;
+        }
+    }
+}
--- WaitStaffApplicataion/FoodMenu.cs
+++ WaitStaffApplicataion/FoodMenu.cs
@@ -2,5 +2,21 @@
 {
     internal class FoodMenu
     {
+        FoodItem[] items;
+        
+        void setMenu()
+        {
+
+        }
+
+        FoodItem getMenuItem(int id)
+        {
+            return items[id];
+        }
+
+        bool getItemAvaliablity(int id)
+        {
+            return items[id].getAvaliability();
+        }
     }
 }
