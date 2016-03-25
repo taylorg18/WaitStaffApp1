280df535ef8a583ac86176f2595712f880aa6528
Alex Reinemann <Reineman18@up.edu>
2016-03-25 14:20:16 -0700

    Added functionality to determine if an item can be sold and returns amount of order not filled without selling any if order cannot be filled

--- WaitStaffApplicataion/FoodItem.cs
+++ WaitStaffApplicataion/FoodItem.cs
@@ -6,6 +6,8 @@
                        sSpecial;
         private float fPrice;
         private bool bAvaliability = true;
+        private int iAmountSold,
+                    iAmountStock;
 
         //constructor for food items
         public FoodItem(string name, string special, float price, bool avaliability)
@@ -14,6 +16,8 @@
             sSpecial = special;
             fPrice = price;
             bAvaliability = avaliability;
+            iAmountSold = 0;
+            iAmountStock = 20;
         }
 
         public void setName(string name)
@@ -44,6 +48,33 @@
         public string getName()
         {
             return sName;
+        }
+
+        /**
+         * determines if item can be sold and returns amount of order not filled without selling any if order cannot be filled
+         **/
+        public int updateSold(int sold)
+        {
+            if(sold <= iAmountStock)
+            {
+                iAmountSold += sold;
+                iAmountStock -= sold;
+                return 0;
+            }
+            else
+            {
+                return sold - iAmountStock;
+            }
+        }
+
+        public int getSold()
+        {
+            return iAmountSold;
+        }
+
+        public int getStock()
+        {
+            return iAmountStock;
         }
 
     }
